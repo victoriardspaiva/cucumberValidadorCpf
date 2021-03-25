f597c1279ccc46f4e113a146d162008f7832ecf0
Victoria Paiva <victoriardspaiva@recife.pe.senac.pe>
2021-03-25 14:06:53 -0300

    subindo o validador cpf

--- src/test/java/io/cucumber/victoria/ValidadorCpfSteps.java
+++ src/test/java/io/cucumber/victoria/ValidadorCpfSteps.java
@@ -7,8 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.cucumber.java.pt.Quando;
-import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.*;
 
 public class ValidadorCpfSteps {
 
